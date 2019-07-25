39c81fcd95624b01c847aa6ff8f4793b261235f1
Bryan Smith <bryansmith1117@gmail.com>
2019-07-25 15:35:41 -0400

    added junit testing template for psychic type

---
+++ test/bowsmith/fightergame/fighters/PsychicTypeTest.java
@@ -0,0 +1,36 @@
+package bowsmith.fightergame.fighters;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+class PyschicTypeTest {
+
+    @Before
+    void setUp() {
+    }
+
+    @After
+    void tearDown() {
+    }
+
+    @Test
+    void basicAttack() {
+    }
+
+    @Test
+    void specialAttack() {
+    }
+
+    @Test
+    void dealDamage() {
+    }
+
+    @Test
+    void takeDamage() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+}
